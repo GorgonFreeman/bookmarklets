7c5181bfc0752b56f54ab82d4b661c5a48a6c8c6
John Martin <johnmartin@Johns-MacBook-Pro.local>
2022-03-24 11:34:50 +1100

    test bookmarklet

--- scripts.js
+++ scripts.js
@@ -15,36 +15,7 @@ const bookmarklets = [
   {
     title: 'Test',
     script: `
-      function copyToClipboard(str) {
-        const el = document.createElement('textarea');
-        el.value = str;
-        el.setAttribute('readonly', '');
-        el.style.position = 'absolute';
-        el.style.left = '-9999px';
-        document.body.appendChild(el);
-        el.select();
-        document.execCommand('copy');
-        document.body.removeChild(el);
-      };
-
-      const params = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-      });
-
-      const getAndUseProductInfo = async () => {
-        const variantParam = params.variant;
-        const { origin, pathname } = location;
-        const productJSONURL = \`\${ origin }\${ pathname }.json\`;
-        const response = await fetch(productJSONURL);
-        const data = await response.json();
-
-        const variant = data.product.variants.find(v => v.id == variantParam);
-        const { sku } = variant;
-        console.log('SKU of current variant:', sku);
-        copyToClipboard(sku);
-      }
-
-      getAndUseProductInfo();
+      console.log('Hi!');
     `
   }
 ];
