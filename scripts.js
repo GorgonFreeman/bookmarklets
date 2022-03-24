074e005fd26ca2a6810d16c9ee26621240d95ab2
John Martin <johnmartin@Johns-MacBook-Pro.local>
2022-03-24 11:58:03 +1100

    populate bookmarklets

--- scripts.js
+++ scripts.js
@@ -17,8 +17,145 @@ const bookmarklets = [
     script: `
       console.log('Hi!');
     `
+  },
+  {
+    title: 'Get Date',
+    script: `
+      const copyToClipboard = str => {
+      const el = document.createElement('textarea');
+      el.value = str;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+    };
+
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear().toString();
+
+    const formattedDate = \`\${ yyyy }-\${ mm }-\${ dd }\`;
+
+    copyToClipboard(formattedDate);
+    `
+  },
+  {
+    title: 'Config.yml Generator',
+    script: `
+      function copyToClipboard(str) {
+        const el = document.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+      };
+
+      try {
+        const url = new URL(location.href);
+
+        let themeID = localStorage.getItem('configThemeID');
+        let password = localStorage.getItem('configPassword');
+        let store = localStorage.getItem('configStore');
+
+        const onAThemePage = url.href.indexOf('/admin/themes/') !== -1;
+        const onAPrivateAppPage = url.href.indexOf('/admin/apps/private/') !== -1;
+        const onAShopifyPage = url.href.indexOf('.myshopify') !== -1;
+        
+        if (onAShopifyPage) {
+          store = url.host;
+        }
+
+        if (!themeID) {
+          if (onAThemePage) {
+            // We are on a theme page, and we need the theme ID. Seize it!
+            themeID = url.href.split('themes/')[1].split('/')[0];
+
+            window.localStorage.setItem('configThemeID', themeID);
+            console.log('Theme ID ready to use! \( 0v0\)._/ Go to the private app for your password, or click again to enter manually!');
+
+          } else {
+            alert('Go to the Edit Theme page on the appropriate theme to get started');
+          }
+        } else if (!password) {
+          if (onAPrivateAppPage) {
+            // We are on a private app page. Take the password!
+            password = $('[name="private_app_password"]').val();
+          } else {
+            password = prompt("Enter your Theme Kit password - this could be from a private app, an email from the Theme Kit Access app, or a custom app. If it's a private app, go to the private app page and click this bookmarklet again.");
+          }
+          window.localStorage.setItem('configPassword', password);
+          console.log('Password ready to use! \( 0v0\)._/ Making your command now!');
+        }
+
+        if (themeID && password) {
+          window.localStorage.removeItem('configThemeID');
+          window.localStorage.removeItem('configPassword');
+          window.localStorage.removeItem('configStore');
+          console.log('Credentials used and destroyed! \(0w0\)9');
+          const configCreateCommand = \`theme configure --password=\${ password } --store=\${ store } --themeid=\${ themeID }\`;
+          copyToClipboard(configCreateCommand);
+          console.log('Paste this in your terminal:', configCreateCommand);
+        }
+      } catch (error) {
+        console.error(error);
+        alert('Something went wrong - check your console for deets.');
+      }
+    `
+  },
+  {
+    title: 'Get SKU of Current Variant',
+    script: `
+      function copyToClipboard(str) {
+        const el = document.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+      };
+
+      const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+      });
+
+      const getAndUseProductInfo = async () => {
+        const variantParam = params.variant;
+        const { origin, pathname } = location;
+        const productJSONURL = \`\${ origin }\${ pathname }.json\`;
+        const response = await fetch(productJSONURL);
+        const data = await response.json();
+
+        const variant = data.product.variants.find(v => v.id == variantParam);
+        const { sku } = variant;
+        console.log('SKU of current variant:', sku);
+        copyToClipboard(sku);
+      }
+
+      getAndUseProductInfo();
+    `
   }
 ];
+
+/*
+  {
+    title: 'Title',
+    script: `
+      console.log('hi!');
+
+    `
+  }
+*/
 
 const html = bookmarklets.map(b => {
   const { title, script } = b;
