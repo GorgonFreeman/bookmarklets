1f5012cdc2fdddbd912ab772d5b302312a909274
John <29110105+GorgonFreeman@users.noreply.github.com>
2022-07-20 13:48:19 +1000

    image ripper: add xmlns to svgs a different way

--- scripts.js
+++ scripts.js
@@ -369,10 +369,10 @@ const bookmarklets = [
 
         // If it was an svg, download that.
         if (nodeName === 'svg') {
+          let svgMarkup = el.outerHTML;
           if (!el.attributes.xmlns) {
-            el.attributes.xmlns = 'http://www.w3.org/2000/svg'
+            svgMarkup = svgMarkup.replace(/svg/, 'svg xmlns="http://www.w3.org"');
           }
-          const svgMarkup = el.outerHTML;
           download('enjoy_your_svg.svg', svgMarkup, false);
           return true;
         }
