aaa4f6c2627764f379c3f325d915cf170e7f7367
John <29110105+GorgonFreeman@users.noreply.github.com>
2022-11-03 16:43:21 +1100

    add px reporter

--- scripts.js
+++ scripts.js
@@ -611,6 +611,27 @@ const bookmarklets = [
     version: '1.0',
     category: 3
   },
+  {
+    title: 'Current px',
+    script: () => {
+      const copyToClipboard = str => {
+        const el = document.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+      };
+
+      copyToClipboard(`${ window.innerWidth }px:`);
+    },
+    docs: '',
+    version: '1.0',
+    category: 1
+  },
 ];
 
 /*
